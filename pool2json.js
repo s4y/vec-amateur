9e94d451bd063e17d19c7fcce24ec83e9c546d59
Sidney San Martín <s@sidneysm.com>
2012-01-25 00:12:35 -0500

    Comment out debugging messages

--- pool2json.js
+++ pool2json.js
@@ -5,16 +5,16 @@ var questionText = fs.readFileSync(process.argv[2], 'utf8');
 var subelements = [], subelement, group, question;
 questionText.split('\n').forEach(function(line){
 	var match;
-	console.error('LINE', line);
+	// console.error('LINE', line);
 	if (question) {
 		if (!question.question){
-			console.error('QUESTION DESCRIPTION', line);
+			// console.error('QUESTION DESCRIPTION', line);
 			question.question = line;
 		} else if ((match = line.match(/^[A-D]. (.+)\s*$/))){
-			console.error('QUESTION ANSWER', match[1]);
+			// console.error('QUESTION ANSWER', match[1]);
 			question.answers.push(match[1]);
 		} else if (/~~\s*/.test(line)) {
-			console.error('END QUESTION');
+			// console.error('END QUESTION');
 			question = null;
 		} else if (question.answers.length) {
 			question.answers[question.answers.length - 1] += ' ' + line;
@@ -23,7 +23,7 @@ questionText.split('\n').forEach(function(line){
 		}
 	} else if (/^\s*$/g.test(line)) {
 	} else if ((match = line.match(/^(?:SUBELEMENT )?([A-Z]\d+) [-–—] ([^\]]+) \[(\d+) Exam Questions [-–—] (\d+) Groups\]\s*$/i))) {
-		console.error('SUBELEMENT', match[2]);
+		// console.error('SUBELEMENT', match[2]);
 		subelements.push(subelement = {
 			id: match[1],
 			name: match[2],
@@ -32,18 +32,18 @@ questionText.split('\n').forEach(function(line){
 			groups: []
 		});
 	} else if ((match = line.match(/^([A-Z]\d+[A-Z]+) (?:[-–—] )?(.+)\s*$/))) {
-		console.error('GROUP', match[2]);
+		// console.error('GROUP', match[2]);
 		subelement.groups.push(group = {
 			id: match[1],
 			name: match[2],
 			questions: []
 		});
 	} else if (/^[A-Z]\d+[A-Z]+\d+ *\([A-D]\) *Question withdrawn/i.test(line)) {
-		console.error('WITHDRAWN QUESTION');
+		// console.error('WITHDRAWN QUESTION');
 		// /dev/null this question
 		question = { answers: [] };
 	} else if ((match = line.match(/^([A-Z]\d+[A-Z]+\d+) *\(([A-D])\)(?: \[([^\]]+)\]?)?\s*$/))) {
-		console.error('START QUESTION');
+		// console.error('START QUESTION');
 		group.questions.push(question = {
 			id: match[1],
 			correctAnswer: { A: 0, B: 1, C: 2, D: 3 }[match[2]],
@@ -51,7 +51,7 @@ questionText.split('\n').forEach(function(line){
 			answers: []
 		});
 	} else {
-		console.error('UNKNOWN LINE', line)
+		console.error('Unknown line:', line)
 	}
 });
 process.stdout.write(JSON.stringify(subelements, null, '\t'));
